b38040260dd08c844426e6d692d278bfc7be1463
Baran Koç <19240000971@ogrenci.ege.edu.tr>
2025-03-02 14:13:54 +0300

    Update 12.Soru.py

--- Project Euler/12.Soru.py
+++ Project Euler/12.Soru.py
@@ -1,7 +1,10 @@
+# bekleme garantili çözüm
+
 sayi = 1
 bolen = 0
 
-while bolen < 500: 
+while bolen < 50: 
+    print(sayi)
     bolen = 0
     sayi += 1
     for i in range(1,sayi+1):
@@ -11,3 +14,19 @@ while bolen < 500:
 print(sayi)
 
 
+# matematik kullanarak teknik geliştirdiğimiz rahatlatıcı çözüm
+
+n = 1
+triangle_number = 1
+while True:
+    count = 0
+    for i in range(1, int(triangle_number**0.5) + 1):
+        if triangle_number % i == 0:
+            count += 2
+    if int(triangle_number**0.5) ** 2 == triangle_number:
+        count -= 1
+    if count > 500:
+        print(triangle_number)
+        break
+    n += 1
+    triangle_number += n
